de814588049c24b37188ca74a05bf23b5d5d369c
tgac1123581321 <tgac.1123581321@gmail.com>
2026-04-20 23:26:23 -0600

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,17 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./config";
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8IY_Wbh33pdqaaY2kKO54CGifi-fEdPs",
+  authDomain: "web-218500.firebaseapp.com",
+  projectId: "web-218500",
+  storageBucket: "web-218500.appspot.com",
+  messagingSenderId: "539802319607",
+  appId: "1:539802319607:web:dc04682efd3ffa857531f2"
+};
+
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
